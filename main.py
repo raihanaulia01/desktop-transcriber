bb0e72e892504114a9c841fb38f1bd7e22a43063
raihan <rahmanraihanaulia@gmail.com>
2026-05-01 17:00:44 +0700

    somewhat  working?

--- main.py
+++ main.py
@@ -1,22 +1,39 @@
 from faster_whisper import WhisperModel
+import soundcard as sc
+import keyboard
+import threading
+import queue
 import os
+
+SAMPLE_RATE = 24000
+CHUNK_SECONDS = 5
+
+default_speaker = sc.default_speaker()
+print(f"Using {default_speaker}")
+mic = sc.get_microphone(default_speaker.id, include_loopback=True)
 
 model_size = "medium"
 print(f"Preparing model {model_size}...")
 model = WhisperModel(model_size, device="cuda", compute_type="int8_float16")
 
-def test_sample():
-    # sample source: https://www.mitsue.co.jp/english/service/audio_and_video/audio_production/narrators_sample.html
-    sample_files = os.listdir("sample_audio")
-    print(f"Sample audio files: {sample_files}")
-    for audio_file in sample_files:
-        audio_path = f"sample_audio/{audio_file}"
-        print(f"Transcribing {audio_path}")
-        segments, info = model.transcribe(audio_path, beam_size=5, task="translate")
-        print("    Detected language '%s' with probability %f" % (info.language, info.language_probability))
+audio_queue = queue.Queue()
 
+def transcribe_worker():
+    while True:
+        audio_mono = audio_queue.get()
+        if audio_mono is None:
+            break
+        segments, info = model.transcribe(audio_mono, beam_size=5, task="translate")
         for segment in segments:
             print("    [%.2fs -> %.2fs] %s" % (segment.start, segment.end, segment.text))
-        print()
 
-test_sample()
+threading.Thread(target=transcribe_worker, daemon=True).start()
+
+print("\nRecording... (press Q to stop)")
+with mic.recorder(samplerate=SAMPLE_RATE) as recorder:
+    while not keyboard.is_pressed("q"):
+        audio = recorder.record(numframes=SAMPLE_RATE * CHUNK_SECONDS)
+        audio_queue.put(audio[:, 0])
+
+audio_queue.put(None)  # signal transcribe worker to stop
+print("Recording stopped.")
