93dcfe665d5bd833ff85345c0cba253c86a2c338
raihan <rahmanraihanaulia@gmail.com>
2026-05-02 09:49:36 +0700

    verify model arguments before using

--- main.py
+++ main.py
@@ -29,12 +29,14 @@ def print_aligned_transcribe(timestamp, text):
 script_dir = os.path.dirname(os.path.abspath(__file__))
 default_path = os.path.join(script_dir, f"rms_values_{datetime.now().strftime("%Y%m%d_%H%M%S")}.csv")
 
+valid_models = ["tiny.en","tiny","base.en","base","small.en","small","medium.en","medium","large-v1","large-v2","large-v3","large","distil-large-v2","distil-medium.en","distil-small.en","distil-large-v3","distil-large-v3.5","large-v3-turbo","turbo"]
+
 parser = argparse.ArgumentParser(description="Live transcriber using faster_whisper by SYSTRAN")
 parser.add_argument("--export-rms-values", action="store_true", help="Exports the rms values to a file in the script's directory.")
 parser.add_argument("--silence-duration", type=float, default=2.0, help="Seconds of silence before processing audio (default: 2.0)")
 parser.add_argument("--silence-threshold", type=float, default=0.01, help="RMS threshold to consider a chunk silent (default: 0.01)")
 parser.add_argument("--max-buffer-duration", type=int, default=20, help="Max seconds of audio to buffer before forcing processing (default: 20)")
-parser.add_argument("--model", type=str, default="medium", help="Set the faster_whisper model, defaults to medium. Please check the vram requirements for each model before using.")
+parser.add_argument("--model", type=str, default="medium", choices=valid_models, help="Set the faster_whisper model, defaults to medium. Please check the vram requirements for each model before using.")
 parser.add_argument("--language", type=str, default=None, help="Language code (e.g. 'en', 'fr', 'ja'). If omitted, language will be auto-detected.")
 arguments = parser.parse_args()
 
