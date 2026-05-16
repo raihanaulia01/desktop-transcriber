e0a6dbb3d3ad818d7bba328336eb201c09164633
raihan <rahmanraihanaulia@gmail.com>
2026-05-16 21:47:32 +0700

    adjust defaults

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ parser = argparse.ArgumentParser(description="Live transcriber using faster_whis
 parser.add_argument("--export-rms-values", action="store_true", help="Exports the rms values to a file in the script's directory.")
 parser.add_argument("--export-transcribed", action="store_true", help="Exports the transcribed text to a file in the scrip's directory.")
 parser.add_argument("--silence-duration", type=float, default=0.5, help="Seconds of silence before processing audio (default: 0.5).")
-parser.add_argument("--vad-threshold", type=restricted_float, default=0.5, help="Silero VAD speech probability sensitivity threshold between 0.0 and 1.0 (default: 0.5).")
+parser.add_argument("--vad-threshold", type=restricted_float, default=0.45, help="Silero VAD speech probability sensitivity threshold between 0.0 and 1.0 (default: 0.45).")
 parser.add_argument("--speech-pad", type=int, default=50, help="Padding around speech in ms (default: 50).")
 parser.add_argument("--max-buffer-duration", type=int, default=20, help="Max seconds of audio to buffer before forcing processing (default: 20).")
 parser.add_argument("--model", type=str, default="medium", choices=VALID_MODELS, help="Set the faster_whisper model, defaults to medium. Please check the vram requirements for each model before using.")
